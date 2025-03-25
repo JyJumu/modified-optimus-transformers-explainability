deba3f29ec12aabc38f0275a6b94319e34b4d8e8
JyJumu <118355179+JyJumu@users.noreply.github.com>
2025-03-25 12:40:24 +0100

    Update myModel.py

--- myModel.py
+++ myModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 from torch import tensor
 from torch.utils.data import Dataset as TDataset
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, BertModel
 
 
 class MyDataset(TDataset):
